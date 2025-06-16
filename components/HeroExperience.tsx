d9820482a785ad999bcf94b8545ec8b297c90ee1
MD. Abu Bakar Siddique <140780598+Abs-Futy7@users.noreply.github.com>
2025-06-16 21:45:22 +0600

    Fix: Updated HeroExperience section

--- components/HeroExperience.tsx
+++ components/HeroExperience.tsx
@@ -1,23 +1,22 @@
 "use client";
 
+
 import { Canvas } from "@react-three/fiber";
 import { 
   OrbitControls, 
   PerspectiveCamera, 
-  useHelper, 
-  SpotLight,
-  PointLight,
   BakeShadows
 } from "@react-three/drei";
-import { Suspense, useRef } from "react";
+import { Suspense} from "react";
 import { Robot } from "./Robot";
-import * as THREE from "three";
 import { useMediaQuery } from "react-responsive";
 
+interface HeroExperienceProps {
+  isMobile: boolean;
+}
+export default function HeroExperience({isMobile}: HeroExperienceProps) {
 
-export default function HeroExperience() {
-
-  const isMobile = useMediaQuery({ maxWidth: 835 });
+  const isMobileQuery = useMediaQuery({ maxWidth: 835 });
 
   return (
     <div className="w-full h-full">
@@ -47,17 +46,17 @@ export default function HeroExperience() {
           {/* 3D Model */}
           <Robot
             rotation={[0.2, Math.PI / 4, 0.1]}
-            scale={isMobile ? 1 : 1}
-            position={isMobile ? [0, -2.2, 0] : [0, -1.2, 0]}
+            scale={isMobileQuery ? 1 : 1}
+            position={isMobileQuery ? [0, -2.2, 0] : [0, -1.2, 0]}
           />
           
           {/* Controls */}
           <OrbitControls
-            enableZoom={isMobile ? true : false}
+            enableZoom={isMobileQuery ? true : false}
             minPolarAngle={Math.PI / 4}
             maxPolarAngle={Math.PI / 2}
             autoRotate
-            autoRotateSpeed={isMobile ? 3 : 2.8}
+            autoRotateSpeed={isMobileQuery ? 3 : 2.8}
           />
           
           <BakeShadows />
